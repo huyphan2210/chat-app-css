6fe6efa8724b6072569ab1d9c37fd5e085537f8e
Huy Phan <huy.phan2210@outlook.com>
2024-03-07 09:28:15 +0700

    Add comment images

--- src/components/Phone/Comment/Comment.vue
+++ src/components/Phone/Comment/Comment.vue
@@ -4,12 +4,24 @@ export default {
   props: {
     content: String,
     isRight: Boolean,
+    imgs: Array<string>,
   },
 };
 </script>
 
 <template>
-  <p :class="isRight ? 'chat-right' : 'chat-left'">{{ content }}</p>
+  <p v-if="!!content" :class="isRight ? 'chat-right' : 'chat-left'">
+    {{ content }}
+  </p>
+  <div v-if="!!imgs" class="chat-images" :class="isRight ? 'right' : 'left'">
+    <img
+      v-for="(img, i) in imgs"
+      :key="i"
+      :src="img"
+      loading="lazy"
+      :alt="'Dog' + i"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -31,6 +43,22 @@ p {
     background-color: white;
     border-radius: 2rem 2rem 0 2rem;
     margin-left: calc(40% - 2rem);
+  }
+}
+
+.chat-images {
+  display: flex;
+  gap: 1rem;
+  width: 70%;
+  margin-bottom: 1rem;
+
+  &.right {
+    margin-left: 30%;
+  }
+
+  img {
+    width: calc((100% - 2rem) / 3);
+    border-radius: 1rem;
   }
 }
 </style>
