6a2b8bc602a65ffe542803d231bcac3b724fb0f5
Huy Phan <huy.phan2210@outlook.com>
2024-03-07 09:28:24 +0700

    Add main headings and paragraph

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@ import Phone from "./components/Phone/Phone.vue";
     <special-shape />
     <div class="content">
       <phone />
+      <hgroup>
+        <h1>Simple Booking</h1>
+        <p>
+          Stay in touch with our dog walkers through the chat interface. This
+          makes it easy to discuss arrangements and make bookings. Once the walk
+          has been completed you can rate your walker and book again all through
+          the chat.
+        </p>
+      </hgroup>
     </div>
   </main>
 </template>
@@ -23,6 +32,25 @@ main {
     width: 100%;
     display: flex;
     justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    hgroup {
+      margin-top: 4rem;
+      text-align: center;
+
+      h1 {
+        color: var(--main-heading);
+        margin-top: 0;
+        font-size: clamp(2rem, 10vw, 4rem);
+      }
+
+      p {
+        line-height: clamp(1.5rem,8vw, 3rem);
+        color: var(--paragraph);
+        font-size: clamp(1rem, 5vw, 2rem);
+      }
+    }
   }
 }
 </style>
